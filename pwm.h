ff798e7fd66e9785468fb5d861418f7b5708a458
magarcia1 <magarcia1@email.arizona.edu>
2015-03-23 11:54:36 -0700

    PWM and ADC added

---
+++ pwm.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   pwm.h
+ * Author: MiguelAnguel
+ *
+ * Created on March 23, 2015, 11:51 AM
+ */
+
+#ifndef PWM_H
+#define	PWM_H
+
+void initPWM();
+
+#endif	/* PWM_H */
+
---
+++ timer.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   timer.h
+ * Author: MiguelAnguel
+ *
+ * Created on March 23, 2015, 11:42 AM
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+void delayUs(unsigned int delay);
+void delayMs(unsigned int delay);
+void initTimer1();
+
+#endif	/* TIMER_H */
+
